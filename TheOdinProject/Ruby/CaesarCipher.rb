b2af1039e2804a32ade7884f44812b680d82ea67
unknown <cary@linux.com>
2018-03-12 08:50:37 -0500

    +

---
+++ TheOdinProject/Ruby/CaesarCipher.rb
@@ -0,0 +1,20 @@
+def caesar(input,shift)
+  input = input.split(//)
+  letters = ('a'..'z').to_a
+  cipher = []
+
+  input.each { |letter|
+  	if letters.include? letter
+	    cipher.push(letters[(letters.index(letter)+shift)%letters.size])
+	  elsif letters.include? letter.downcase
+	    cipher.push(letters[(letters.index(letter.downcase)+shift)%letters.size].upcase)
+	  else
+	    cipher.push(letter)
+
+	  end
+  }
+  puts cipher.join
+
+end
+
+caesar("What a string!",5)
